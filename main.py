fa72fedc7cae50171d981bfcf6d4e05993e435d7
mode-durachyo <144421362+mode-durachyo@users.noreply.github.com>
2023-09-18 16:18:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-count = int(input())
-for k in range(count+1):
-    print(k)
-    k += 1
+list = ["1.1", "2.2", "3.3"]
+
+def sum (arr):
+    total = 0
+
+    for num in arr:
+        total += float(num)
+
+    return total
+
+print(sum(list))
